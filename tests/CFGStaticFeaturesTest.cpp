e33c077098c8e1744f087bdc051b82623b1962b0
Yukio Siraichi <sir.yukio@gmail.com>
2016-03-11 03:17:47 -0300

    Tests for CFG's feature extractor added.

--- tests/CFGStaticFeaturesTest.cpp
+++ tests/CFGStaticFeaturesTest.cpp
@@ -1,7 +1,16 @@
 #include "gtest/gtest.h"
 
+#include "pinhao/Features/Feature.h"
+#include "pinhao/Features/FeatureRegistry.h"
+
 using namespace pinhao;
 
-TEST(CFGStaticFeaturesTest, SimpleTest) {
+TEST(CFGStaticFeaturesTest, GetFeatures) {
+  std::unique_ptr<Feature> ModuleFeatures = FeatureRegistry::get("cfg_md_static");
+  ASSERT_NE(ModuleFeatures.get(), nullptr);
+  std::unique_ptr<Feature> FunctionFeatures = FeatureRegistry::get("cfg_fn_static");
+  ASSERT_NE(FunctionFeatures.get(), nullptr);
+  std::unique_ptr<Feature> BasicBlockFeatures = FeatureRegistry::get("cfg_bb_static");
+  ASSERT_NE(BasicBlockFeatures.get(), nullptr);
 }
 
