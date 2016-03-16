5168cd0a6e48ab976595b0793c66e395ef157d1f
Yukio Siraichi <sir.yukio@gmail.com>
2016-03-16 11:28:38 -0300

    Implemented more tests for the FeatureSetTest.

--- tests/FeatureSetTest.cpp
+++ tests/FeatureSetTest.cpp
@@ -2,12 +2,18 @@
 
 #include "pinhao/Features/FeatureSet.h"
 
+#include "llvm/PassRegistry.h"
+
 #include <memory>
 
 using namespace pinhao;
 
 TEST(FeatureSetTest, EmptyInstantiation) {
-  std::unique_ptr<FeatureSet> Set = FeatureSet::get();
+
+  std::shared_ptr<FeatureSet> Set(FeatureSet::get().release());
   ASSERT_NE(Set.get(), nullptr);
+
+  FeatureSetWrapperPass *FSWP = new FeatureSetWrapperPass(&Set);
+  ASSERT_NE(FSWP, nullptr);
 }
 
