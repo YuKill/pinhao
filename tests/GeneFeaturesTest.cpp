a3b98cd562955580c2783015bbaf410ed428d42a
Yukio Siraichi <sir.yukio@gmail.com>
2016-03-26 19:35:54 -0300

    Added tests for GeneFeatures.

---
+++ tests/GeneFeaturesTest.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+
+#include "pinhao/Features/Feature.h"
+#include "pinhao/Features/FeatureRegistry.h"
+
+#include "ModuleReader.h"
+
+using namespace pinhao;
+
+TEST(GeneFeaturesTest, FunctionsGeneFeatureTest) {
+  std::string Benchmark("../../benchmark/polybench-ll/2mm/2mm.bc");
+  ModuleReader Reader(Benchmark);
+  
+  std::unique_ptr<Feature> FunctionsGene = FeatureRegistry::get("function-dna");
+  ASSERT_NE(FunctionsGene.get(), nullptr);
+
+  FunctionsGene->processModule(*(Reader.getModule().get()));
+  FunctionsGene->printYaml();
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
