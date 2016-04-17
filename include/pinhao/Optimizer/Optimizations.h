1be33cb0f817d7a54b3b816ef48db2f9b8114546
Yukio Siraichi <sir.yukio@gmail.com>
2016-04-17 02:23:42 -0300

    Separated optimizations list and its public methods from other classes.

---
+++ include/pinhao/Optimizer/Optimizations.h
@@ -0,0 +1,188 @@
+/*-------------------------- PINHAO project --------------------------*/
+
+/**
+ * @file Optimizations.h
+ */
+
+#ifndef PINHAO_OPTIMIZATIONS_H
+#define PINHAO_OPTIMIZATIONS_H
+
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace pinhao {
+  class OptimizationSet;
+  class OptimizationSequence;
+
+  void initializeOptimizer();
+
+  /**
+   * @brief Enumerator of the available optimizations.
+   */
+  enum class Optimization {
+    adce = 0,
+    alwaysInline,
+    argpromotion,
+    bbVectorize,
+    breakCritEdges,
+    codegenprepare,
+    constmerge,
+    constprop,
+    dce,
+    deadargelim,
+    die,
+    dse,
+    functionattrs,
+    globaldce,
+    globalopt,
+    gvn,
+    indvars,
+    inlineOpt,
+    instcombine,
+    internalize,
+    ipconstprop,
+    ipsccp,
+    jumpThreading,
+    lcssa,
+    licm,
+    loopDeletion,
+    loopExtract,
+    loopExtractSingle,
+    loopReduce,
+    loopRotate,
+    loopSimplify,
+    loopUnroll,
+    loopUnswitch,
+    loweratomic,
+    lowerinvoke,
+    lowerswitch,
+    mem2reg,
+    memcpyopt,
+    mergefunc,
+    mergereturn,
+    partialInliner,
+    pruneEh,
+    reassociate,
+    reg2mem,
+    scalarrepl,
+    sccp,
+    simplifycfg,
+    sink,
+    strip,
+    stripDeadDebugInfo,
+    stripDeadPrototypes,
+    stripDebugDeclare,
+    stripNondebug,
+    tailcallelim
+  };
+
+  /**
+   * Vector with the name of the optimizations.
+   */
+  static const std::vector<std::string> Optimizations = {
+    "adce",
+    "always-inline",
+    "argpromotion",
+    "bb-vectorize",
+    "break-crit-edges",
+    "codegenprepare",
+    "constmerge",
+    "constprop",
+    "dce",
+    "deadargelim",
+    "die",
+    "dse",
+    "functionattrs",
+    "globaldce",
+    "globalopt",
+    "gvn",
+    "indvars",
+    "inline",
+    "instcombine",
+    "internalize",
+    "ipconstprop",
+    "ipsccp",
+    "jump-threading",
+    "lcssa",
+    "licm",
+    "loop-deletion",
+    "loop-extract",
+    "loop-extract-single",
+    "loop-reduce",
+    "loop-rotate",
+    "loop-simplify",
+    "loop-unroll",
+    "loop-unswitch",
+    "loweratomic",
+    "lowerinvoke",
+    "lowerswitch",
+    "mem2reg",
+    "memcpyopt",
+    "mergefunc",
+    "mergereturn",
+    "partial-inliner",
+    "prune-eh",
+    "reassociate",
+    "reg2mem",
+    "scalarrepl",
+    "sccp",
+    "simplifycfg",
+    "sink",
+    "strip",
+    "strip-dead-debug-info",
+    "strip-dead-prototypes",
+    "strip-debug-declare",
+    "strip-nondebug",
+    "tailcallelim"
+  };
+
+  /// @brief Gets a @a Optimization corresponding to the @a OptName.
+  Optimization getOptimization(std::string OptName);
+  /// @brief Gets the optimization name corresponding to the @a Opt.
+  std::string getOptimizationName(Optimization Opt);
+
+  /// @brief Gets the @a PassKind of the @a Opt.
+  int getPassKind(Optimization Opt);
+
+  /** 
+   * @brief Applies a generated @a OptimizationSequence to a module, from the @a OptimizationSet.
+   * @return The optimized module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, OptimizationSet *Set);
+  /** 
+   * @brief Applies the @a OptimizationSequence to a module.
+   * @return The optimized module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, OptimizationSequence *Seq);
+  /** 
+   * @brief Applies a generated @a OptimizationSequence to a function which has name @a FunctionName, 
+   * from the @a OptimizationSet.
+   *
+   * @return The optimized module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, std::string FunctionName, OptimizationSet *Set);
+  /** 
+   * @brief Applies the @a OptimizationSequence to a function whose name is @a FunctionName.
+   * @return The optimized module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, std::string FunctionName, OptimizationSequence *Seq);
+  /** 
+   * @brief Applies a generated @a OptimizationSequence to the function @a Function, from the @a OptimizationSet.
+   * @return The optimized module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, llvm::Function *Function, OptimizationSet *Set);
+  /**
+   * @brief Applies the @a OptimizationSequence to a function.
+   * @return The optimized function's module.
+   */
+  llvm::Module *applyOptimizations(llvm::Module &Module, llvm::Function *Function, OptimizationSequence *Seq);
+
+}
+
+#endif
+
+
---
+++ lib/Optimizer/Optimizations.cpp
@@ -0,0 +1,147 @@
+/*-------------------------- PINHAO project --------------------------*/
+
+/**
+ * @file Optimizations.cpp
+ */
+
+#include "pinhao/Optimizer/Optimizations.h"
+#include "pinhao/Optimizer/OptimizationSet.h"
+#include "pinhao/Optimizer/OptimizationSequence.h"
+
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Bitcode/ReaderWriter.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Vectorize.h"
+#include "llvm/Transforms/IPO.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <algorithm>
+
+using namespace pinhao;
+
+void pinhao::initializeOptimizer() {
+  llvm::PassRegistry *Registry = llvm::PassRegistry::getPassRegistry();
+  llvm::initializeCore(*Registry);
+  llvm::initializeTransformUtils(*Registry);
+  llvm::initializeScalarOpts(*Registry);
+  llvm::initializeInstCombine(*Registry);
+  llvm::initializeAnalysis(*Registry);
+  llvm::initializeVectorization(*Registry);
+  llvm::initializeIPO(*Registry);
+  llvm::initializeCodeGen(*Registry);
+}
+
+Optimization pinhao::getOptimization(std::string OptName) {
+  uint64_t Count = 0;
+  for (auto S : Optimizations) {
+    if (S == OptName) break;
+    ++Count;
+  }
+  assert(Count != Optimizations.size() && "There is no such optimization name.");
+  return static_cast<Optimization>(Count);
+}
+
+std::string pinhao::getOptimizationName(Optimization Opt) {
+  return Optimizations[static_cast<int>(Opt)];
+}
+
+int pinhao::getPassKind(Optimization Opt) {
+  int PassKind;
+  llvm::PassRegistry *Registry = llvm::PassRegistry::getPassRegistry();
+  const llvm::PassInfo *PI = Registry->getPassInfo(getOptimizationName(Opt));
+  llvm::Pass *Pass = PI->createPass();
+  PassKind = Pass->getPassKind();
+  delete Pass;
+  return PassKind;
+}
+
+static void printModule(llvm::Module *Module, std::string Path) {
+  std::error_code Err;
+  llvm::raw_fd_ostream *Out = new llvm::raw_fd_ostream(Path, Err, llvm::sys::fs::OpenFlags::F_RW);
+  llvm::WriteBitcodeToFile(Module, *Out);   
+  Out->flush();
+  delete Out;
+}
+
+static llvm::Module *readModule(std::string Path) {
+  llvm::LLVMContext &Context = llvm::getGlobalContext();
+  llvm::SMDiagnostic Error;
+  return llvm::parseIRFile(Path, Error, Context).release();
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, OptimizationSet *Set) {
+  Set->generateRandomSequenceIfNone(); 
+  return applyOptimizations(Module, Set->DefaultSequence);
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, llvm::Function *Function, OptimizationSet *Set) {
+  Set->generateRandomSequenceIfNone(); 
+  return applyOptimizations(Module, Function, Set->DefaultSequence);
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, std::string FunctionName, OptimizationSet *Set) {
+  Set->generateRandomSequenceIfNone(); 
+  return applyOptimizations(Module, Module.getFunction(FunctionName), Set->DefaultSequence);
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, OptimizationSequence *Sequence) {
+  std::string TmpName = ".tmp-" + std::to_string(time(0));
+
+  fflush(stdout);
+  pid_t Pid = fork();
+
+  if (Pid == 0) {
+    llvm::legacy::PassManager PM; 
+    Sequence->populatePassManager(PM);
+    PM.run(Module);
+
+    printModule(&Module, TmpName);
+    exit(0);
+  }
+
+  int Return;
+  waitpid(Pid, &Return, 0); 
+  if (Return != 0) {
+    std::cerr << "Failed when applying optimizations." << std::endl;
+    return nullptr;
+  }
+
+  return readModule(TmpName);
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, std::string FunctionName, OptimizationSequence *Sequence) {
+  return applyOptimizations(Module, Module.getFunction(FunctionName), Sequence);
+}
+
+llvm::Module *pinhao::applyOptimizations(llvm::Module &Module, llvm::Function *Function, OptimizationSequence *Sequence) {
+  std::string TmpName = ".tmp-" + std::to_string(time(0));
+
+  fflush(stdout);
+  pid_t Pid = fork();
+  if (Pid == 0) {
+    llvm::legacy::FunctionPassManager FPM(&Module);
+    Sequence->populateFunctionPassManager(FPM);
+    FPM.run(*Function);
+
+    printModule(&Module, TmpName);
+    exit(0);
+  }
+
+  int Return;
+  waitpid(Pid, &Return, 0);
+  if (Return != 0) {
+    std::cerr << "Failed when applying optimizations." << std::endl;
+    return nullptr;
+  }
+
+  return readModule(TmpName);
+}
