8f7a60e157bb6257fb7e755513c64642189c768d
Yukio Siraichi <sir.yukio@gmail.com>
2016-04-25 18:22:37 -0300

    Change initialization header name to Initialization.h

--- include/pinhao/Initialization.h
+++ include/pinhao/Initialization.h
@@ -7,7 +7,11 @@
 #ifndef PINHAO_INITIALIZE_ROUTINES_H
 #define PINHAO_INITIALIZE_ROUTINES_H
 
+#include "llvm/IR/Module.h"
+
 namespace pinhao {
+
+  void initializeOptimizer();
 
   void initializeGEOSFeatures(llvm::Module&);
   void initializeGEOSBasicBlockFreqFeature(llvm::Module&);
