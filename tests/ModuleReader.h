fdbd9ea35b6c500015e9bbb0089793158d951250
Yukio Siraichi <sir.yukio@gmail.com>
2016-04-25 18:27:16 -0300

    Removed 'using namespace llvm' from ModuleReader header.

--- tests/ModuleReader.h
+++ tests/ModuleReader.h
@@ -8,20 +8,18 @@
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/IR/LLVMContext.h"
 
-using namespace llvm;
-
 class ModuleReader {
   private:
-    std::shared_ptr<Module> M;
+    std::shared_ptr<llvm::Module> M;
 
   public:
     ModuleReader(std::string &ModuleName) {
-      LLVMContext &Context = getGlobalContext();
-      SMDiagnostic Error;
-      M = parseIRFile(ModuleName, Error, Context);
+      llvm::LLVMContext &Context = llvm::getGlobalContext();
+      llvm::SMDiagnostic Error;
+      M = llvm::parseIRFile(ModuleName, Error, Context);
     }
 
-    std::shared_ptr<Module> getModule() { return M; }
+    std::shared_ptr<llvm::Module> getModule() { return M; }
 };
 
 #endif
