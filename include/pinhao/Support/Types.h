7afdf1aae0c7229290b73ec719fcc01c3cd61f0a
Yukio Siraichi <sir.yukio@gmail.com>
2016-04-26 23:54:19 -0300

    Creating objects with template based on ValueType.

--- include/pinhao/Support/Types.h
+++ include/pinhao/Support/Types.h
@@ -19,6 +19,23 @@ namespace pinhao {
     String
   };
 
+  template <class T>
+    T *createTyped(ValueType Type) {
+      switch (Type) {
+        case ValueType::Int:    return new T<int>();
+        case ValueType::Float:  return new T<double>(); 
+        case ValueType::Bool:   return new T<bool>();
+        case ValueType::String: return new T<std::string>();
+      }
+    }
+
+  template <class T>
+    ValueType getValueTypeFor() { return 0; }
+
+  template<> ValueType getValueTypeFor<int>()         { return ValueType::Int; }
+  template<> ValueType getValueTypeFor<double>()      { return ValueType::Float; }
+  template<> ValueType getValueTypeFor<std::string>() { return ValueType::String; }
+
 }
 
 #endif
