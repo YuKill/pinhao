c2d43bdfe6b82dddf0392412257cb74fc3b20f1d
Yukio Siraichi <sir.yukio@gmail.com>
2016-04-28 03:42:51 -0300

    Feature implementation moved to Feature.cpp. Modified for YAMLWrapper.

--- include/pinhao/Features/Feature.h
+++ include/pinhao/Features/Feature.h
@@ -40,7 +40,6 @@ namespace pinhao {
       };
 
     protected:
-      std::shared_ptr<YamlfyBase> Yaml;
       std::shared_ptr<FeatureInfo> Info;
       bool Processed;
 
@@ -51,84 +50,56 @@ namespace pinhao {
       virtual ~Feature() {};
 
       /// @brief Constructs a @a Feature with the information @a FeatInfo.
-      Feature(FeatureInfo* InfoPtr, FeatureKind FKind) : 
-        Info(std::shared_ptr<FeatureInfo>(InfoPtr)), Kind(FKind) { Processed = false; }
+      Feature(FeatureInfo* InfoPtr, FeatureKind FKind);
 
       /// @brief Gets the name of the feature.
-      std::string getName() const { return Info->getName(); }
+      std::string getName() const;
 
       /// @brief Gets the description of the feature.
-      std::string getDescription() const { return Info->getDescription(); }
+      std::string getDescription() const;
 
       /// @brief Returns true if there is a sub-feature with name @a SubFeatureName.
-      bool hasSubFeature (std::string SubFeatureName) const { 
-        if (!isComposite()) return false;
-        CompositeFeatureInfo *CompInfo = static_cast<CompositeFeatureInfo*>(Info.get());
-        return CompInfo->hasSubFeature(SubFeatureName); 
-      }
+      bool hasSubFeature (std::string SubFeatureName) const;
 
       /// @brief Gets the description of the sub-feature with name @a SubFeatureName.
-      std::string getSubFeatureDescription(std::string SubFeatureName) { 
-        assert(hasSubFeature(SubFeatureName) && "This feature does not have the sub-feature desired.");
-        CompositeFeatureInfo *CompInfo = static_cast<CompositeFeatureInfo*>(Info.get());
-        return CompInfo->getSubFeatureDescription(SubFeatureName); 
-      }
+      std::string getSubFeatureDescription(std::string SubFeatureName);
 
       /// @brief Returns true if the feature has more than two sub-features.
-      bool isComposite() const { return Info->isComposite(); }
-
+      bool isComposite() const;
       /// @brief Returns true if the feature has already been processed.
-      bool isProcessed() const { return Processed; }
-
-      bool isLinear() const { return Kind == LinearKind; }
-      bool isMapped() const { return Kind == MappedKind; }
-
+      bool isProcessed() const;
+      /// @brief Returns true if it is a @a LinearFeature.
+      bool isLinear() const;
+      /// @brief Returns true if it is a @a MappedFeature.
+      bool isMapped() const;
       /// @brief Returns true if it is a static feature.
-      bool isStaticFeature() const { return Info->isStaticFeature(); }
-
+      bool isStaticFeature() const;
       /// @brief Returns true if it is a dynamic feature.
-      bool isDynamicFeature() const { return Info->isDynamicFeature(); }
+      bool isDynamicFeature() const;
 
       /// @brief Gets the @a begin iterator of the @a Info.
-      iterator begin() {
-        assert(isComposite() && "This feature is not a composite type feature."); 
-        CompositeFeatureInfo *CompInfo = static_cast<CompositeFeatureInfo*>(Info.get());
-        return CompInfo->begin();
-      }
-
+      iterator begin() const;
       /// @brief Gets the @a end iterator of the @a Info.
-      iterator end() {
-        assert(isComposite() && "This feature is not a composite type feature."); 
-        CompositeFeatureInfo *CompInfo = static_cast<CompositeFeatureInfo*>(Info.get());
-        return CompInfo->end();
-      }
+      iterator end() const;
 
       /**
        * @brief Prints its information into a @a std::ostream object in the format yaml.
        *
        * @param Out The output stream that the function shall print.
        */
-      void printYaml(std::ostream &Out = std::cout, bool printReduced = true) {
-        YAML::Emitter Emitter(Out); 
-        appendYaml(Emitter, printReduced);
-        Out << std::endl;
-      }
+      void print(std::ostream &Out = std::cout);
 
       /**
        * @brief Appends to a @a YAML::Emitter this feature data.
        *
        * @param Emitter The place where the data information will be appended.
        */
-      virtual void appendYaml(YAML::Emitter &Emitter, bool PrintReduced = true) {
-        Yaml->append(Emitter, PrintReduced); 
-      }
+      virtual void append(YAML::Emitter &Emitter) const = 0; 
 
       /**
        * @brief Gets the feature data from a @a YAML::Node.
        */
-      virtual void getFromYamlNode(YAML::Node &Node) {
-        Yaml->get(Node); 
-      }
+      virtual void get(const YAML::Node &Node) = 0;
 
       /**
        * @brief Gets the necessary information from a llvm::Module.
